c847ea635fbffa584df7a4db492aca4fd65ca171
ambrishba-sacumen <ambrish.ba@sacumen.com>
2023-04-27 22:33:37 +0530

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+
+    public static  void  main(String[] args)
+    {
+        String s="java";
+        String[] arr=s.split("");
+        for (int i=0;i< arr.length;i++)
+        {
+            
+                System.out.println(arr[i]);
+
+        }
+    }
+}
